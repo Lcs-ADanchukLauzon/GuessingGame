07facdcbb7d48ca68eae38adb380cd0f21acfd9e
Danchuk-Lauzon, Alexy <adanchuklauzon@lcs-mba3080.lcs.on.ca>
2018-01-16 13:57:04 -0500

    Outlets and Actions

--- GuessingGame/ViewController.swift
+++ GuessingGame/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARC Outlets
+    @IBOutlet weak var numberGuessed: UITextField!
+    
+    
+    // MARC Properties (variables)
+    
+    
+    // MARC Overrides
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    // MARC: Actions
+    @IBAction func guessSubmited(_ sender: Any) {
+        // Print the number that was guessed
+        print(numberGuessed.text)
+        
+        // use a guard statement to bind to a non-optional variable
+        guard let inputGiven = numberGuessed.text else {
+          // if the input was nil, stop and return (exit) the function
+            return
+        }
+        
+        // use a guard statement to attempt to create an integer
+        guard let intagerGiven = Int(inputGiven) else {
+           // if the input was text (e.g. "five" we can't make an integer so stop and return (exit) the function)
+            return
+        }
+        
+    }
+    
+    
+    // MARC: Custom function(s)
 }
 
